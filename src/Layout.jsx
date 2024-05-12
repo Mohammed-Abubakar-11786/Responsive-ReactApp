4e8de97ee4f33e83497bd509aa87ac199a9ce0db
Mohammed-Abubakar-11786 <mohdabubakar.11786@gmail.com>
2024-05-12 19:19:49 +0530

    testing

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,13 @@
-import { Outlet } from "react-router-dom";
+/* import { Outlet } from "react-router-dom"; */
+import Home from "./Components/Home/Home"; /* 
 import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
+import Footer from "./Components/Footer/Footer"; */
 
 function Layout() {
   return (
     <>
       <div className="">
-        <Header />
-        <main>
-          <Outlet />
-        </main>
-        <Footer />
+        <Home />
       </div>
       {/* <div className="min-[455px]:hidden">
         <p className="font-bold text-red-500 text-4xl px-10 py-5">
